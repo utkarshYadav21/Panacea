01eae32fa87b46046e1c29b80f14e1ca03a793cc
utkarshYadav21 <utkarshyadav329@gmail.com>
2024-02-22 00:47:06 +0530

    test

--- backend/db/config.js
+++ backend/db/config.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const dbUrl =
-  "mongodb+srv://utkarshyadav329:22utkarsh22@cluster0.w14eg7b.mongodb.net/Panacea";
+  "Mongodb url";
 mongoose
   .connect(dbUrl, {
     useNewUrlParser: true,
